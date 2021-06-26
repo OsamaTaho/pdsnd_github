b9b14ccd47dfea48c153bad0500e3508d388204f
OsamaTaho <46663426+OsamaTaho@users.noreply.github.com>
2021-06-26 20:46:05 +0300

    add docstring to explain a {dict} role

--- bikeshare.py
+++ bikeshare.py
@@ -253,6 +253,7 @@ def check_for_day():
         day='Sunday'
     return day.title()
 
+"""Added this dict to decide which month to return, receive num and return string for better user experience(?)"""
 months={1:'January',2:'February',3:'March',4:'April',5:'May',6:'June',7:'July',8:'August',9:'September',10:'October',11:'November',12:'December'}
 
 
